d07aeebc45ed3393ed3636352efc3b709d2d8562
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-08-03 19:19:54 +0530

    Modularized the code for user auth

---
+++ routes/auth.js
@@ -0,0 +1,66 @@
+import express from "express";
+import pool from "../db.js";
+import jwt from "jsonwebtoken";
+
+const authRouter = express.Router();
+
+authRouter.post("/register", async (request, response) => {
+  const { username, password } = request.body;
+
+  try {
+    const userExist = await pool.query(
+      "SELECT * FROM users WHERE username = $1",
+      [username]
+    );
+    if (userExist.rows.length > 0) {
+      return response.status(400).send("Username already exists");
+    } else {
+      await pool.query(
+        "INSERT INTO users (username, password) VALUES ($1, $2)",
+        [username, password]
+      );
+
+      response.status(201).send("Register successful");
+    }
+  } catch (error) {
+    console.log("Error at registration: " + error);
+    response.status(500).send("Error at registration");
+  }
+});
+
+authRouter.post("/login", async (request, response) => {
+  const { username, password } = request.body;
+
+  const userExist = await pool.query(
+    "SELECT * FROM users WHERE username = $1",
+    [username]
+  );
+  if (userExist.rows.length > 0) {
+    if (password === userExist.rows[0].password) {
+      const token = jwt.sign(
+        { id: userExist.rows[0].id, username: userExist.rows[0].username },
+        process.env.JWT_SECRET,
+        { expiresIn: "10m" }
+      );
+
+      response.cookie("token", token, { httpOnly: true, secure: false });
+
+      return response.status(201).send("Login successful");
+    } else {
+      return response.send("Wrong password daw");
+    }
+  } else {
+    return response.status(400).send("Invalid username or password");
+  }
+});
+
+authRouter.post("/logout", async (request, response) => {
+  response.clearCookie("token", {
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax",
+  });
+  response.sendStatus(200);
+});
+
+export default authRouter;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
 import pool from "./db.js";
-import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authenticateToken from "./authenticateToken.js";
+import authRouter from "./routes/auth.js";
 
 dotenv.config();
 
@@ -17,6 +17,7 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
+app.use("/", authRouter);
 
 try {
   pool.query(`
@@ -34,64 +35,15 @@ app.get("/", (request, response) => {
   response.send("Hello, world");
 });
 
-app.post("/register", async (request, response) => {
-  const { username, password } = request.body;
+// app.post("/register", async (request, response) => {
 
-  try {
-    const userExist = await pool.query(
-      "SELECT * FROM users WHERE username = $1",
-      [username]
-    );
-    if (userExist.rows.length > 0) {
-      return response.status(400).send("Username already exists");
-    } else {
-      await pool.query(
-        "INSERT INTO users (username, password) VALUES ($1, $2)",
-        [username, password]
-      );
+// });
 
-      response.status(201).send("Register successful");
-    }
-  } catch (error) {
-    console.log("Error at registration: " + error);
-    response.status(500).send("Error at registration");
-  }
-});
+// app.post("/login", async (request, response) => {});
 
-app.post("/login", async (request, response) => {
-  const { username, password } = request.body;
+// app.post("/logout", (request, response) => {
 
-  const userExist = await pool.query(
-    "SELECT * FROM users WHERE username = $1",
-    [username]
-  );
-  if (userExist.rows.length > 0) {
-    if (password === userExist.rows[0].password) {
-      const token = jwt.sign(
-        { id: userExist.rows[0].id, username: userExist.rows[0].username },
-        process.env.JWT_SECRET,
-        { expiresIn: "10m" }
-      );
-
-      response.cookie("token", token, { httpOnly: true, secure: false });
-
-      return response.status(201).send("Login successful");
-    } else {
-      return response.send("Wrong password daw");
-    }
-  } else {
-    return response.status(400).send("Invalid username or password");
-  }
-});
-
-app.post("/logout", (request, response) => {
-  response.clearCookie("token", {
-    httpOnly: true,
-    secure: false,
-    sameSite: "lax",
-  });
-  response.sendStatus(200);
-});
+// });
 
 app.get("/me", authenticateToken, (request, response) => {
   response.json({ user: request.user });
