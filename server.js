e346fdfc30f03229120fb6ad61833a60fb9b37db
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-08-03 19:12:51 +0530

    Minor changes

--- server.js
+++ server.js
@@ -6,11 +6,17 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authenticateToken from "./authenticateToken.js";
 
+dotenv.config();
+
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
-dotenv.config();
 
 try {
   pool.query(`
@@ -78,7 +84,16 @@ app.post("/login", async (request, response) => {
   }
 });
 
-app.post("/me", authenticateToken, (request, response) => {
+app.post("/logout", (request, response) => {
+  response.clearCookie("token", {
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax",
+  });
+  response.sendStatus(200);
+});
+
+app.get("/me", authenticateToken, (request, response) => {
   response.json({ user: request.user });
 });
 
