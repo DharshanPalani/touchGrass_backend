8c8c405416e63d2505cddd60b979ec4f7b805c32
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-08-03 20:32:33 +0530

    Added token for search

--- routes/user/user.js
+++ routes/user/user.js
@@ -1,9 +1,11 @@
 import express from "express";
 import pool from "../../db.js";
+import authenticateToken from "../../authenticateToken.js";
 
 const userRouter = express.Router();
 
-userRouter.get("/:username", async (request, response) => {
+userRouter.get("/:username", authenticateToken, async (request, response) => {
+  const user = request.user;
   const { username } = request.params;
 
   const userProfileExist = await pool.query(
@@ -12,9 +14,11 @@ userRouter.get("/:username", async (request, response) => {
   );
 
   if (userProfileExist.rows.length > 0) {
+    if (userProfileExist.rows[0].username === user.username) {
+      return response.send("Daw it's you! touch grass GET OU-");
+    }
     response.send(
-      (await userProfileExist).rows[0].username +
-        " exists, this is their profile btw!"
+      userProfileExist.rows[0].username + " exists, this is their profile btw!"
     );
   } else {
     response.send("No person like that exist! Touch some grass GET OU-");
